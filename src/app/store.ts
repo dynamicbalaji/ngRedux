5f268b0420b4cbb4f55108d3027b92a9b298e1c1
Balaji Ashokkumar <31545514+dynamicbalaji@users.noreply.github.com>
2017-11-15 23:48:54 +0530

    Adding complex object & optional object in state
    object: { var1, var2 }
    
    var?: number -> question mark colon refers to optional

--- src/app/store.ts
+++ src/app/store.ts
@@ -3,10 +3,16 @@ import { INCREMENT } from './actions';
 // Determines the state and properties of our store
 export interface IAppState {
     counter: number;
+    messaging?: {
+        newMsgs: number;
+    }
 }
 
 export const INITIAL_STATE: IAppState = {
-    counter: 0
+    counter: 0,
+    messaging: {
+        newMsgs: 6
+    }
 }
 
 export function rootReducer(state: IAppState, action): IAppState {
